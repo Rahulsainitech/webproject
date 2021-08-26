a24328a581a1fa1a7679abffae92be54a00cbe01
Rahulsainitech <66263277+Rahulsainitech@users.noreply.github.com>
2021-08-26 07:27:43 +0530

    error404

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const app=express();
 const bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+app.use(express.static('dance_website/static/img'));
 mongoose.connect('mongodb://localhost:27017/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
 const port = process.env.PORT || 80;
 
